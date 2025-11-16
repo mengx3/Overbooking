9be404f526a7df522506b4a8539d37bee51d4e8a
mengx3 <156154923+mengx3@users.noreply.github.com>
2025-11-15 21:34:03 -0500

    Update 01_schema.sql

--- sql/01_schema.sql
+++ sql/01_schema.sql
@@ -40,3 +40,30 @@ CREATE TABLE flight_stats (
         REFERENCES flight(flight_id, scenario_id)
         ON DELETE CASCADE
 );
+
+CREATE TABLE scenario_stats (
+    scenario_id        INTEGER NOT NULL REFERENCES scenario(scenario_id)
+                        ON DELETE CASCADE,
+    policy_id          INTEGER NOT NULL REFERENCES policy(policy_id)
+                        ON DELETE CASCADE,
+    total_bumped       INTEGER NOT NULL CHECK (total_bumped >= 0),
+    total_cost         NUMERIC(12,2) NOT NULL CHECK (total_cost >= 0),
+    bumping_rate       DOUBLE PRECISION NOT NULL CHECK (bumping_rate >= 0),
+    flights_affected   INTEGER NOT NULL CHECK (flights_affected >= 0),
+
+    PRIMARY KEY (scenario_id, policy_id)
+);
+
+
+CREATE TABLE risk_event (
+    event_id      VARCHAR(100) PRIMARY KEY,
+    scenario_id   INTEGER NOT NULL REFERENCES scenario(scenario_id)
+                  ON DELETE CASCADE,
+    flight_id     VARCHAR(50) NOT NULL,
+
+    label         TEXT,
+
+    FOREIGN KEY (flight_id, scenario_id)
+        REFERENCES flight(flight_id, scenario_id)
+        ON DELETE CASCADE
+);
