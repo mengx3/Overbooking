9cf2cfbee04ae14d9694d35c4cb9f27496df15e9
mengx3 <156154923+mengx3@users.noreply.github.com>
2025-11-15 21:33:24 -0500

    Update 01_schema.sql

--- sql/01_schema.sql
+++ sql/01_schema.sql
@@ -5,3 +5,22 @@ DROP TABLE IF EXISTS flight CASCADE;
 DROP TABLE IF EXISTS scenario_stats CASCADE;
 DROP TABLE IF EXISTS scenario CASCADE;
 DROP TABLE IF EXISTS policy CASCADE;
+
+
+CREATE TABLE policy (
+    policy_id      SERIAL PRIMARY KEY,
+    policy_name    VARCHAR(50) UNIQUE NOT NULL
+);
+
+CREATE TABLE scenario (
+    scenario_id    INTEGER PRIMARY KEY,
+    description    TEXT
+);
+
+CREATE TABLE flight (
+    flight_id      VARCHAR(50) NOT NULL,
+    scenario_id    INTEGER NOT NULL REFERENCES scenario(scenario_id)
+                   ON DELETE CASCADE,
+    capacity       INTEGER NOT NULL CHECK (capacity > 0),
+    PRIMARY KEY (flight_id, scenario_id)
+);
