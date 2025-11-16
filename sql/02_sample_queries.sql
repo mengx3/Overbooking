0e1bee140357e5200a2c255bbc03099ac7e7e1a7
mengx3 <156154923+mengx3@users.noreply.github.com>
2025-11-15 21:47:04 -0500

    Create 02_sample_queries.sql

---
+++ sql/02_sample_queries.sql
@@ -0,0 +1,9 @@
+--Q1
+SELECT
+    p.policy_name,
+    AVG(s.total_bumped) AS avg_bumped,
+    AVG(s.total_cost)   AS avg_cost
+FROM scenario_stats s
+JOIN policy p ON s.policy_id = p.policy_id
+GROUP BY p.policy_name
+ORDER BY avg_cost;
